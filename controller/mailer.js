6706897682d1e79f5888427066ac614a1c8721ec
BCrandom <coreabarbara306@gmail.com>
2025-06-19 16:27:53 -0400

    Cambios en index, sobre nosotros, contacto, y simulacion de newsletter

--- controller/mailer.js
+++ controller/mailer.js
@@ -1,9 +1,10 @@
 const nodemailer = require("nodemailer");
+require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-        user: process.env.GMAIL_USER, // Render inyecta estas variables
+        user: process.env.GMAIL_USER,
         pass: process.env.GMAIL_APP_PASSWORD,
     },
 });
